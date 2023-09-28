1b482589537988bc1c167fbe35151f0e57c6b109
RobertRuai <robertruai4@gmail.com>
2023-09-28 18:10:44 +0300

    abstract classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) { throw Error("Class     extending Building must override evacuationWarningMessage") }
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {}
+}
