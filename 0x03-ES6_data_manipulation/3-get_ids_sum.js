71f5f9b25ee327f1df6010de8a726c45ed45b2d5
RobertRuai <robertruai4@gmail.com>
2023-10-02 19:14:04 +0300

    a function getStudentIdsSum that returns the sum of all the student ids.

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
-const getStudentIdsSum = (listOfStudents) => {
+const getStudentIdsSum = (listOfStudents) => (
   listOfStudents.reduce((accumulator, st) => accumulator + st.id, 0)
-};
+);
 
 export default getStudentIdsSum;
