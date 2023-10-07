05326f93ad3b5813545dffc97feb316ed96ecc05
RobertRuai <robertruai4@gmail.com>
2023-10-07 19:58:28 +0300

    tried my best this project was hard

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,16 @@
+export interface MajorCredits {
+  credits: number;
+  brand: string;
+}
+export interface MinorCredits {
+  credits: number;
+  brand: string;
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MajorCredits" }
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits, brand: "MinorCredits" }
+}
