1e586b90cfebcad6ce5cf979c3ac6e8ce302f6ad
RobertRuai <robertruai4@gmail.com>
2023-11-21 20:40:41 +0300

    requirement check

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -21,6 +21,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const app = server.listen(port, host);
+const app = server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
 
 module.exports = app;
