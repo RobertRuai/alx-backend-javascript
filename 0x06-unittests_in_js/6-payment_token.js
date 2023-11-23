3e5d3b5aaf5634804b878573cfa470d2932ba822
RobertRuai <robertruai4@gmail.com>
2023-11-23 18:34:55 +0300

    6. Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,7 @@
 function getPaymentTokenFromAPI (success) {
   if (success === true) {
     return new Promise(function(resolve){
-      resolve({data: 'Successful response from the API' });
+      resolve({ data: 'Successful response from the API' });
     });
   }
 }
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,7 +4,7 @@ const  getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('test result is true', (done) => {
     getPaymentTokenFromAPI(true).then((res) => {
-      expect(res).to.equal({data: 'Successful response from the API' });
+      expect(res).to.equal({ data: 'Successful response from the API' });
 	  });
     done();
   });
